9700d14f007567f1569e2fbb989cd21a943af9ad
Tom Montgomery <montgomery3681@gmail.com>
2025-01-11 18:27:40 -0700

    Initial Commit

---
+++ Onboard/OnboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardApp.swift
+//  Onboard
+//
+//  Created by tom montgomery on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct OnboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OnboardTests/OnboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardTests.swift
+//  OnboardTests
+//
+//  Created by tom montgomery on 1/11/25.
+//
+
+import Testing
+@testable import Onboard
+
+struct OnboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
