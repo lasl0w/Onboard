3d0478f9e315d306df1060bd89fef52e22444713
Tom Montgomery <montgomery3681@gmail.com>
2025-02-13 20:44:40 -0800

    - add gradients - add multi-step flow - add content to flow - fix previews

--- Onboard/MultiStep/MultiStepOnboardFlow.swift
+++ Onboard/MultiStep/MultiStepOnboardFlow.swift
@@ -14,45 +14,41 @@ struct MultiStepOnboardFlow: View {
     
     var body: some View {
         
-        TabView {
-            // multiple steps is as simple as boom boom boom!
-            OnboardingView(title: "Step 1", desc: "Did you know that you can get 300% smarter by getting a good night's sleep?", image: "")
-                .tabItem {
-                    Image(systemName: "1.circle")
-                    Text("Step 1")
-                }
-            OnboardingView(title: "Step 2", desc: "Did you know that you can get 300% smarter by getting a good night's sleep?", image: "")
-                .tabItem {
-                    Image(systemName: "2.circle")
-                    Text("Step 2")
-                }
-            OnboardingView(title: "Step 3", desc: "Did you know that you can get 300% smarter by getting a good night's sleep?", image: "")
-                .overlay(alignment: .bottom) {
-                                Button(action: {
-                                    // set onboarded flag to true
-                                    onboarded = true
-                                }, label: {
-                                    Text("Get Started")
-                                        .bold()
-                                        .padding()
-                                    // foreground Style, not font
-                                        .foregroundStyle(Color.white)
-                                    // background padding set to a comomon button size by default
-                                        .background(Color.blue)
-                                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                                })
+        ZStack {
+            // set the color for the entire view by adding it to a zstack. all steps.
+            Color("MyBackground")
+                .ignoresSafeArea(edges: .all)
+            TabView {
+                // multiple steps is as simple as boom boom boom!
+                OnboardingView(title: "Restful sleep will give you superpowers", desc: "Did you know that you can get 300% smarter by getting a good night's sleep?", image: "onboardingart1")
+
+                OnboardingView(title: "Feel happier and more productive", desc: "Did you know that you can get 300% smarter by getting a good night's sleep?", image: "onboardingart2")
+
+                OnboardingView(title: "Deepen your relationships", desc: "Did you know that you can get 300% smarter by getting a good night's sleep?", image: "onboardingart3")
+                    .overlay(alignment: .bottom) {
+                        Button(action: {
+                            // set onboarded flag to true
+                            onboarded = true
+                        }, label: {
+                            Text("Get Started")
+                                .bold()
                                 .padding()
-                }
-                .tabItem {
-                    Image(systemName: "3.circle")
-                    Text("Step 3")
-                }
-            // LAST TAB HAS A BUTTON
-            /// multiple options
-            /// 1) hide the button in the OnboardingView, but only show it if the tabItem tag is 3
-            /// 2) do a .overlay mod on Step 3
+                            // foreground Style, not font
+                                .foregroundStyle(Color.white)
+                            // background padding set to a comomon button size by default
+                                .background(Color.blue)
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                        })
+                        .padding(.bottom, 50)
+                    }
+
+                // LAST TAB HAS A BUTTON
+                /// multiple options
+                /// 1) hide the button in the OnboardingView, but only show it if the tabItem tag is 3
+                /// 2) do a .overlay mod on Step 3
+            }
+            .tabViewStyle(PageTabViewStyle())
         }
-        .tabViewStyle(PageTabViewStyle())
         // default tabview style is bottom nav tabs, but we want onboardy swipes
     }
 }
--- Onboard/OnboardingView.swift
+++ Onboard/OnboardingView.swift
@@ -18,21 +18,63 @@ struct OnboardingView: View {
     var image: String?
     
     var body: some View {
-        VStack {
-            Spacer()
-            Text(title)
-                .font(.largeTitle)
-            Text(desc)
-                .multilineTextAlignment(.center)
-            //"Did you know that you can get 300% smarter by getting a good night's sleep?"
-            Spacer()
+        
+        // think of it as building the layers - image on top of the gradient
+        ZStack {
+            // bottom layer = image
+            VStack {
+                Image(image ?? "")
+                    .padding(.top, 70)
+                // padding is low enough so we can see it being melded with the gradient & good text overlap
 
+                Spacer()
+                // the 2 to 1 spacer technique. moving it up 1/3 of the way. still overlaps some. DONT DO THAT, instead....
+                // use a single space, then add padding so that it's consistent across screensizes
                 
+            }
+
+            
+            // Gradient - top left to bottom right diagonal - (0,0) to (1,1)
+//            LinearGradient(colors: [Color.green, Color.blue], startPoint: UnitPoint(x: 0, y: 0), endPoint: UnitPoint(x: 1, y: 1))
+//                .ignoresSafeArea()
+            LinearGradient(colors: [Color.clear, Color.myBackground], startPoint: UnitPoint(x: 0, y: 0), endPoint: UnitPoint(x: 1, y: 1))
+                .ignoresSafeArea(edges: .all)
+            
+            // LinearGradient with stops = finer control of gradient transition
+//            LinearGradient(stops: [
+//                Gradient.Stop(color: Color.red, location: 0.2),
+//                Gradient.Stop(color: Color.blue, location: 0.4),
+//                Gradient.Stop(color: Color.purple, location: 0.9),
+//            ],
+//                           startPoint: UnitPoint(x: 0, y: 0),
+//                           endPoint: UnitPoint(x: 1, y: 1))
+//            .ignoresSafeArea(edges: .all)
+            
+            // Text
+            VStack {
+                Spacer()
+                Text(title)
+                    .font(.largeTitle)
+                    .bold()
+                    .padding(.top, 140)
+                    .foregroundStyle(.indigo)
+                Text(desc)
+                    .padding(.top, 10)
+                //.multilineTextAlignment(.center)
+                //"Did you know that you can get 300% smarter by getting a good night's sleep?"
+                Spacer()
+
+            }
+            .padding(.horizontal, 20)
+            .multilineTextAlignment(.center)
+            
         }
+        
+
     }
 }
 
-//#Preview {
-//    // for the sake of the preview, we pass in false
-//    OnboardingView(onboarded: Binding.constant(false))
-//}
+#Preview {
+    // for the sake of the preview, we pass in false
+    OnboardingView(title: "Restful sleep will give you superpowers", desc: "Take our 7 day sleep challenge to learn how to fall asleep faster and stay asleep longer", image: "onboardingart1")
+}
